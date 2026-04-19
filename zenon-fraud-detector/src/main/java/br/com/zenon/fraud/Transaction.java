ba40163ffbad05bb4c3c434396f07040a02034ea
Wilson <wlfcoelho@gmail.com>
2026-04-18 22:28:40 -0300

    Criação das validação dos valores do arquivo

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
@@ -1,6 +1,8 @@
 package br.com.zenon.fraud;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+
 
 public record Transaction(
         int step,
@@ -14,4 +16,46 @@ public record Transaction(
         BigDecimal newbalanceDest,
         Boolean isFraud,
         Boolean isFlaggedFraud) {
+
+    public Transaction {
+
+        Objects.requireNonNull(type, "type should not be null");
+        Objects.requireNonNull(amount, "amount should not be null");
+        Objects.requireNonNull(nameOrig, "nameOrig should not be null");
+        Objects.requireNonNull(oldbalanceOrg, "oldbalanceOrg should not be null");
+        Objects.requireNonNull(newbalanceOrig, "newbalanceOrig should not be null");
+        Objects.requireNonNull(nameDest, "nameDest should not be null");
+        Objects.requireNonNull(oldbalanceDest, "oldbalanceDest should not be null");
+        Objects.requireNonNull(newbalanceDest, "newbalanceDest should not be null");
+        Objects.requireNonNull(isFraud, "isFraud should not be null");
+        Objects.requireNonNull(isFlaggedFraud, "isFlaggedFraud should not be null");
+
+        if (step <= 0) {
+            throw new IllegalArgumentException("step should be positive: " + step);
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("amount should be positive: " + amount);
+        }
+        if (oldbalanceOrg.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("amount should be positive: " + oldbalanceOrg);
+        }
+        if (newbalanceOrig.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("amount should be positive: " + newbalanceOrig);
+        }
+        if (oldbalanceDest.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("amount should be positive: " + oldbalanceDest);
+        }
+        if (newbalanceDest.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("amount should be positive: " + newbalanceDest);
+        }
+
+        if (nameOrig.isBlank()) {
+            throw new IllegalArgumentException("name should not be empty: " + nameOrig);
+        }
+        if (nameDest.isBlank()) {
+            throw new IllegalArgumentException("name should not be empty: " + nameDest);
+        }
+    }
 }
+
