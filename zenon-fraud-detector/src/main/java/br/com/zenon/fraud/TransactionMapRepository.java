09dce8364b296951d8a9d5326f3c4cc95bae5f30
Wilson <wlfcoelho@gmail.com>
2026-05-27 23:53:00 -0300

    Refatorando o codigo

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionMapRepository.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionMapRepository.java
@@ -1,25 +1,28 @@
-/*
 package br.com.zenon.fraud;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TransactionMapRepository implements TransactionRepository {
 
-    private final Map<String, Transaction> transactionMap;
+    private final Map<String, Transaction> transactionsMap;
 
     public TransactionMapRepository(List<Transaction> transactions) {
-        this.transactionMap = new HashMap<>();
-        for (Transaction transaction : transactions) {
-            transactionMap.put(transaction.nameOrig(), transaction);
-        }
+        Objects.requireNonNull(transactions);
+        this.transactionsMap =
+                transactions.stream().
+                        collect(Collectors.toMap(
+                                transaction -> transaction.customerOrigin().getName(),
+                                //Mesma função no caso abaixo os dois devolvem a transação acima.
+                                Function.identity()));
+                                // transaction -> transaction));
     }
 
     @Override
-    public Optional<Transaction> findByNameOrig(String originName) {
-        return Optional.ofNullable(transactionMap.get(originName));
+    public Optional<Transaction> findByOriginName(String originName) {
+        return Optional.ofNullable(transactionsMap.get(originName));
     }
 }
-*/
+
+
